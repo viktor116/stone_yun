6369c708b38182c14bac459e7c3491b5f7b6d4e1
98414 <984149293@qq.com>
2024-11-11 21:40:53 +0800

    11.11

--- src/main/java/com/soybean/items/ItemsRegister.java
+++ src/main/java/com/soybean/items/ItemsRegister.java
@@ -5,6 +5,7 @@ import com.soybean.config.InitValue;
 import com.soybean.items.custom.GrassItem;
 import com.soybean.items.custom.InvertMinecartItem;
 import com.soybean.items.custom.MinecartHatItem;
+import com.soybean.items.custom.WheatItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
@@ -31,7 +32,11 @@ public class ItemsRegister {
     public static final Item INVERT_MINECART = register(new InvertMinecartItem(new Item.Settings().maxCount(1)), "invert_minecart");
     public static final Item MINECART_HAT = register(new MinecartHatItem(), "minecart_hat");
     public static final Item GRASS = register(new GrassItem(new Item.Settings()),"grass");
-
+    //小麦动
+    public static final Item WHEAT = register(new WheatItem(new Item.Settings()),"wheat");
+    public static final Item STONE_SWORD = register(new SwordItem(ToolMaterials.STONE,new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.STONE, 3, -2.4F)).maxCount(1)),"stone_sword");
+    public static final Item STONE_AXE = register(new AxeItem(ToolMaterials.STONE,new Item.Settings().attributeModifiers(AxeItem.createAttributeModifiers(ToolMaterials.STONE, 7F, -3.2F)).maxCount(1)),"stone_axe");
+    public static final Item STONE_PICKAXE = register(new PickaxeItem(ToolMaterials.STONE,new Item.Settings().attributeModifiers(PickaxeItem.createAttributeModifiers(ToolMaterials.STONE, 1.0F, -2.8F)).maxCount(1)),"stone_pickaxe");
     public static void initialize() {
         Registry.register(Registries.ITEM_GROUP, FUN_ITEM_GROUP_KEY, ABSTRACT_CUSTOM_ITEM_GROUP);
         ItemGroupEvents.modifyEntriesEvent(FUN_ITEM_GROUP_KEY).register(itemGroup->{
@@ -40,6 +45,10 @@ public class ItemsRegister {
             itemGroup.add(INVERT_MINECART);
             itemGroup.add(MINECART_HAT);
             itemGroup.add(GRASS);
+            itemGroup.add(WHEAT);
+            itemGroup.add(STONE_SWORD);
+            itemGroup.add(STONE_AXE);
+            itemGroup.add(STONE_PICKAXE);
         });
 
     }
---
+++ src/main/java/com/soybean/items/custom/WheatItem.java
@@ -0,0 +1,10 @@
+package com.soybean.items.custom;
+
+import net.minecraft.item.Item;
+
+public class WheatItem extends Item {
+    public WheatItem(Settings settings) {
+        super(settings);
+    }
+
+}
