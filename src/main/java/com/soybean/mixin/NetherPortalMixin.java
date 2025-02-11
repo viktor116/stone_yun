fb26e8f689e4f0f33b9fa5ec317265be45b35af3
98414 <984149293@qq.com>
2025-02-11 22:07:16 +0800

    2/11

--- src/main/java/com/soybean/mixin/NetherPortalMixin.java
+++ src/main/java/com/soybean/mixin/NetherPortalMixin.java
@@ -3,12 +3,14 @@ package com.soybean.mixin;
 import com.soybean.block.ModBlock;
 import net.minecraft.block.*;
 import net.minecraft.client.render.item.ItemModels;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.dimension.NetherPortal;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,61 +21,98 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
+
 @Mixin(NetherPortal.class)
-public class NetherPortalMixin {
+public abstract class NetherPortalMixin {
     @Shadow @Final private WorldAccess world;
-    @Shadow @Final private BlockPos lowerCorner;
-    @Shadow @Final private Direction.Axis axis;
+    @Shadow @Final private static AbstractBlock.ContextPredicate IS_VALID_FRAME_BLOCK;
+    @Shadow private Direction.Axis axis;
+    @Shadow private Direction negativeDir;
+    @Shadow @Nullable
+    private BlockPos lowerCorner;
+    @Shadow private int height;
+    @Shadow private int width;
 
-    // 修改高度检查
-    @Inject(method = "getHeight", at = @At("HEAD"), cancellable = true)
-    private void getHeight(CallbackInfoReturnable<Integer> cir) {
-        if (isSimplePortalValid()) {
-            cir.setReturnValue(1);
-        }
-    }
+    @Shadow
+    public abstract int getPotentialHeight(BlockPos.Mutable pos);
 
-    // 修改宽度检查
+    // 修改 getWidth 方法，允许 1x1 的传送门
     @Inject(method = "getWidth", at = @At("HEAD"), cancellable = true)
     private void getWidth(CallbackInfoReturnable<Integer> cir) {
-        if (isSimplePortalValid()) {
-            cir.setReturnValue(1);
+        int width = getWidth(this.lowerCorner, this.negativeDir);
+        if (width >= 1 && width <= 21) { // 允许最小宽度为 1
+            cir.setReturnValue(width);
         }
     }
 
-    // 修改有效性检查
+    // 修改 getHeight 方法，允许 1x1 的传送门
+    @Inject(method = "getHeight", at = @At("HEAD"), cancellable = true)
+    private void getHeight(CallbackInfoReturnable<Integer> cir) {
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        int height = this.getPotentialHeight(mutable);
+        if (height >= 1 && height <= 21) { // 允许最小高度为 1
+            cir.setReturnValue(height);
+        }
+    }
+
+    // 修改 isValid 方法，支持 1x1 的传送门
     @Inject(method = "isValid", at = @At("HEAD"), cancellable = true)
     private void isValid(CallbackInfoReturnable<Boolean> cir) {
-        if (isSimplePortalValid()) {
+        if (this.lowerCorner != null && this.width >= 1 && this.width <= 21 && this.height >= 1 && this.height <= 21) {
             cir.setReturnValue(true);
         }
     }
 
-    // 修改框架有效性检查
-    @Inject(method = "isHorizontalFrameValid", at = @At("HEAD"), cancellable = true)
-    private void isHorizontalFrameValid(BlockPos.Mutable pos, int height, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
-    }
-
-    // 修改传送门创建逻辑
+    // 修改 createPortal 方法，支持创建 1x1 的传送门
     @Inject(method = "createPortal", at = @At("HEAD"), cancellable = true)
     private void createPortal(CallbackInfo ci) {
-        if (isSimplePortalValid()) {
-            // 根据初始轴向设置传送门方向
-            Direction.Axis portalAxis = (this.axis == Direction.Axis.X) ? Direction.Axis.X : Direction.Axis.Z;
+        BlockState blockState = Blocks.NETHER_PORTAL.getDefaultState().with(NetherPortalBlock.AXIS, this.axis);
+        BlockPos.iterate(this.lowerCorner, this.lowerCorner.offset(Direction.UP, this.height - 1).offset(this.negativeDir, this.width - 1)).forEach((pos) -> {
+            this.world.setBlockState(pos, blockState, 18);
+        });
+        ci.cancel();  // 取消原始的传送门创建逻辑
+    }
 
-            // 设置传送门方块
-            BlockState portalState = Blocks.NETHER_PORTAL.getDefaultState()
-                    .with(NetherPortalBlock.AXIS, portalAxis);
+    // 修改 getLowerCorner 方法，支持 1x1 的传送门
+    @Inject(method = "getLowerCorner", at = @At("HEAD"), cancellable = true)
+    private void getLowerCorner(BlockPos pos, CallbackInfoReturnable<BlockPos> cir) {
+        for (int i = Math.max(this.world.getBottomY(), pos.getY() - 21); pos.getY() > i && validStateInsidePortal(this.world.getBlockState(pos.down())); pos = pos.down()) {
+        }
 
-            this.world.setBlockState(this.lowerCorner, portalState, 18);
-            ci.cancel();
+        Direction direction = this.negativeDir.getOpposite();
+        int j = getWidth(pos, direction) - 1;
+        if (j <= 0) { // 允许 1x1 的传送门
+            cir.setReturnValue(pos);
+        } else {
+            cir.setReturnValue(pos.offset(direction, j));
         }
     }
 
-    // 辅助方法：检查简单传送门是否有效
-    private boolean isSimplePortalValid() {
-        return this.world.getBlockState(this.lowerCorner.up()).isOf(Blocks.OBSIDIAN) &&
-                this.world.getBlockState(this.lowerCorner.down()).isOf(Blocks.OBSIDIAN);
+    // 修改 getWidth 方法，支持宽度为 1
+    private int getWidth(BlockPos pos, Direction direction) {
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+        for (int i = 0; i <= 21; ++i) {
+            mutable.set(pos).move(direction, i);
+            BlockState blockState = this.world.getBlockState(mutable);
+            if (!validStateInsidePortal(blockState)) {
+                if (IS_VALID_FRAME_BLOCK.test(blockState, this.world, mutable)) {
+                    return i;
+                }
+                break;
+            }
+
+            BlockState blockState2 = this.world.getBlockState(mutable.move(Direction.DOWN));
+            if (!IS_VALID_FRAME_BLOCK.test(blockState2, this.world, mutable)) {
+                break;
+            }
+        }
+
+        return 1;  // 修改：确保最低宽度为 1
+    }
+
+    // 你可以添加一些辅助方法，如 validStateInsidePortal 等，来确保其工作正常
+    private static boolean validStateInsidePortal(BlockState state) {
+        return state.isAir() || state.isIn(BlockTags.FIRE) || state.isOf(Blocks.NETHER_PORTAL);
     }
 }
