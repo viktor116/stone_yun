a84acf453301fadefd7647ddc0a157ee743df7de
98414 <984149293@qq.com>
2025-03-26 00:19:34 +0800

    3/26 complete

---
+++ src/main/java/com/soybean/items/item/BlazeEyeItem.java
@@ -0,0 +1,24 @@
+package com.soybean.items.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.EndPortalFrameBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.EnderEyeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+
+public class BlazeEyeItem extends EnderEyeItem {
+
+    public BlazeEyeItem(Settings settings) {
+        super(settings);
+    }
+
+}
